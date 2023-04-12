d93d8fa66687a8f6391619bbb8e176bca1a6b75c
Seba <sevaledesma@gmail.com>
2023-04-11 21:40:24 -0300

    Cambio baseUrl

--- src/app/services/educacion.service.ts
+++ src/app/services/educacion.service.ts
@@ -15,22 +15,27 @@ export class EducacionService {
 
   //Metodo para cargar lista de educaciones.
   public listarEducaciones(){
-    return this.http.get(`${baseUrl}/educacion/`);
+    //return this.http.get(`${baseUrl}/educacion/`);
+    return this.http.get(this.baseUrl + 'educacion/');
   }
   //Metodo para agregar educacion.
   public agregarEducacion(educacion:any){
-    return this.http.post(`${baseUrl}/educacion/`, educacion);
+    //return this.http.post(`${baseUrl}/educacion/`, educacion);
+    return this.http.post(this.baseUrl + 'educacion/', educacion);
   }
   //Metodo para actualizar Experiencia
   public actualizarEducacion(educacion:any){
-    return this.http.put(`${baseUrl}/educacion/`,educacion);
+    //return this.http.put(`${baseUrl}/educacion/`,educacion);
+    return this.http.put(this.baseUrl + 'educacion/',educacion);
   }
   //Metodo para obtener o buscar Educacion por id.
   public obtenerEducacionPorId(educacionId:any){
-    return this.http.get(`${baseUrl}/educacion/${educacionId}`);
+    //return this.http.get(`${baseUrl}/educacion/${educacionId}`);
+    return this.http.get(this.baseUrl + `educacion/${educacionId}`);
   }
   //Metodo para eliminar educacion.    
   public eliminarEducacion(educacionId:any){
-    return this.http.delete(`${baseUrl}/educacion/${educacionId}`);
+    //return this.http.delete(`${baseUrl}/educacion/${educacionId}`);
+    return this.http.delete(this.baseUrl + `educacion/${educacionId}`);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {  
   production: true,
-  URL: 'https://backend-portfolio2.onrender.com'
+  URL: 'https://backend-portfolio2.onrender.com/'
 };
