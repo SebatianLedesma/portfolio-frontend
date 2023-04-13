a092d24f17b9a84dd9ef19b7f6e84a88ebb694fb
Seba <sevaledesma@gmail.com>
2023-04-13 08:47:10 -0300

    Eliminando config.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@
 export const environment = {  
   production: true,
   URL: 'https://backend-portfolio2.onrender.com/'
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   //URL: 'http://localhost:8080/'
   URL: 'https://backend-portfolio2.onrender.com/'
+  
 };
 
 /*
